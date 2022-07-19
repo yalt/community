71e3bba3e184a56b222ae7d8ad8e75b8a0a3266c
tanoxi <2422051586@qq.com>
2022-07-19 22:29:39 +0800

    first commit

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -3,6 +3,7 @@ package com.nowcoder.community.dao;
 import com.nowcoder.community.entity.DiscussPost;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
